210237c5ea8648aa507ed24d78207745871abeb9
Berina Sanjta <berina@sanjta.org>
2021-09-16 15:24:30 +0200

    Adding folder ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main (){
+
+    // only change code below this line
+    var myFirstName = 'Berina';
+    var myLastName = 'Sanjta';
+    var myHomeTown = 'Zenica';
+    // Only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main ());
+
+module.exports = main;
